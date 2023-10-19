79afda26e3110483eaadaad6746af20fa9f71e8d
Shreyas Mocherla <shreyas@shreyasm.com>
2023-10-18 19:04:01 -0500

    Fixed Shortest Process Next scheduler

--- schedulers.cpp
+++ schedulers.cpp
@@ -62,59 +62,49 @@ int RoundRobin(const int& curTime, const vector<Process>& procList, const int& t
 //Shortest Process Next scheduler implementation. In general, this function maintains a double ended queue
 //of processes that are candidates for scheduling (the ready variable) and always schedules
 //the process with the shortest total time needed, if available (i.e., if the list has members)
-int ShortestProcessNext(const int& curTime, const vector<Process>& procList, const int& timeQuantum)
+int ShortestProcessNext(const int& curTime, vector<Process>& procList, const int& timeQuantum)
 {
-    static int timeToNextSched = timeQuantum;  //keeps track of when we should actually schedule a new process
-    static deque<int> ready;  //keeps track of the processes that are ready to be scheduled
+    static int timeToNextSched = timeQuantum; // keeps track of when we should actually schedule a new process
+    static deque<int> ready; // keeps track of the processes that are ready to be scheduled
 
     int idx = -1;
+    int shortestTime = INT_MAX;
 
-    // first look through the process list and find any processes that are newly ready and
-    // add them to the back of the ready queue
-    for(int i = 0, i_end = procList.size(); i < i_end; ++i)
-    {
-        if(procList[i].startTime == curTime)
-        {
+    // First, look through the process list and find any processes that are newly ready and
+    // add them to the back of the ready queue.
+    for (int i = 0, i_end = procList.size(); i < i_end; ++i) {
+        if (procList[i].startTime == curTime) {
             ready.push_back(i);
         }
     }
 
-    // now take a look the head of the ready queue, and update if needed
-    // (i.e., if we are supposed to schedule now or the process is done)
-    if(timeToNextSched == 0 || procList[ready[0]].isDone)
-    {
-        // the process at the start of the ready queue is being taken off of the
-        // processor
-
-        // if the process isn't done, add it to the back of the ready queue
-        if(!procList[ready[0]].isDone)
-        {
-            ready.push_back(ready[0]);
+    // Now, find the process with the shortest remaining time in the ready queue.
+    for (int i : ready) {
+        if (procList[i].totalTimeNeeded < shortestTime && !procList[i].isDone) {
+            idx = i;
+            shortestTime = procList[i].totalTimeNeeded;
         }
-
-        // remove the process from the front of the ready queue and reset the time until
-        // the next scheduling
-        ready.pop_front();
-        timeToNextSched = timeQuantum;
     }
 
-    // if the ready queue has any processes on it
-    if(ready.size() > 0)
-    {
-        // grab the front process and decrement the time to next scheduling
-        idx = ready[0];
-        --timeToNextSched;
-    }
-    // if the ready queue has no processes on it
-    else
-    {
-        // send back an invalid process index and set the time to next scheduling
-        // value so that we try again next time step
-        idx = -1;
+    // Update the scheduling based on the time quantum and process completion.
+    if (idx != -1) {
+        --procList[idx].totalTimeNeeded;
+        if (procList[idx].totalTimeNeeded == 0) {
+            procList[idx].isDone = true;
+            ready.pop_front();
+            timeToNextSched = timeQuantum;
+        } else if (timeToNextSched == 0) {
+            ready.push_back(idx);
+            ready.pop_front();
+            timeToNextSched = timeQuantum;
+        } else {
+            --timeToNextSched;
+        }
+    } else {
         timeToNextSched = 0;
     }
 
-    // return back the index of the process to schedule next
+    // Return the index of the process to schedule next.
     return idx;
 }
 
